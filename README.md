5138245fd2e83e8f09f41b2065341442dcf6629d
Grant Elledge <grant@appcove.com>
2022-11-21 12:05:33 -0500

    working looped page construction

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Jinja Sample
-Super simple example of outputting merged HTML files.
+Super simple example of outputting merged HTML files using the Python [Jinja2](https://jinja.palletsprojects.com/en/3.1.x/) library. Here the `filler#.html` files will be used to populate `mainframe.html`, creating four nearly identical pages. From here, adding content to the filler pages allows for the construction of a simple website.
+
+## Configuration
+1. Jinja is not in the standard Python library, so will need to be installed: `pip install Jinja2`
+2. The sample pages will be built in `/output` after running `./build.py`.
+3. Any additional pages in the root directory would need to be added to the `pages` list in `build.py`.
--- build.py
+++ build.py
@@ -4,27 +4,20 @@ import os
 import os.path
 from jinja2 import Environment, FileSystemLoader
 
+# # Compile a single page with Jinja2
+# environment = Environment(loader = FileSystemLoader(os.getcwd()))
+# template = environment.get_template('./filler1.html')
+# output_page = template.render()
+# with open('output/filler1.html', 'w') as f:
+#     f.write(output_page)
 
-# Compile a single page with Jinja2
+
+# Compile a batch and write them to files with Jinja2
+pages = ['./filler1.html', './filler2.html', './filler3.html', './filler4.html']
 environment = Environment(loader = FileSystemLoader(os.getcwd()))
-template = environment.get_template("./postgreSQL.html")
-pg_page = template.render()
-print(pg_page)
 
-# # Compile the whole batch and write them to files with Jinja2
-# pages = [
-#     {"title": "PostgreSQL", "content": "./PostgreSQL.html"},
-#     {"title": "typeSystem", "content": "./TypeSystem.html"},
-# ]
-
-
-# for i, page in enumerate(pages):
-#     pagename = pages[i]["title"]
-#     pagetext = pages[i]["content"]
-#     content = template.render(
-#         page,
-#         title = pagename
-#         content = pagetext
-#     )
-#     with open(pagename, mode="w", encoding="utf-8") as message:
-#         message.write(content)
+for page in pages:
+    template = environment.get_template(page)
+    output_page = template.render()
+    with open('output/' + page, 'w') as f:
+        f.write(output_page)
